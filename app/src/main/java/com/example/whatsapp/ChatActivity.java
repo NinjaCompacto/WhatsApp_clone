fc445ab3bc4cdbba4968076dd2aafa0a219cd7a5
NinjaCompacto <jvnsouza2003@hotmail.com>
2021-08-31 11:27:28 -0300

    1 - subtuindo info dos usuarios destinatarios 2 - otimizando fragment de contatos

--- app/src/main/java/com/example/whatsapp/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/ChatActivity.java
@@ -1,45 +1,56 @@
 package com.example.whatsapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.example.whatsapp.Model.Usuario;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ChatActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
+        private CircleImageView circleImageChat;
+        private TextView textNome;
+        private Usuario usuariodestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        setSupportActionBar(findViewById(R.id.toolbar));
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_chat);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //Configuração toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        //configurações iniciais
+        textNome = findViewById(R.id.textNomeChat);
+        circleImageChat = findViewById(R.id.circleImageChat);
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_chat);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        //recuperando dados do usuario da intent
+        Bundle bundle = getIntent().getExtras();
+        if (bundle !=  null){
+            usuariodestinatario = (Usuario) bundle.getSerializable("chatcontato");
+            textNome.setText(usuariodestinatario.getNome());
+                if (usuariodestinatario.getFoto() != null){
+                    Uri urlfoto = Uri.parse(usuariodestinatario.getFoto());
+                    Glide.with(getApplication()).load(urlfoto).into(circleImageChat);
+                }
+        }
+
     }
 }
--- app/src/main/java/com/example/whatsapp/Fragments/ContatosFragment.java
+++ app/src/main/java/com/example/whatsapp/Fragments/ContatosFragment.java
@@ -3,7 +3,6 @@ package com.example.whatsapp.Fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -116,7 +115,9 @@ public class ContatosFragment extends Fragment {
                 new RecyclerItemClickListener.OnItemClickListener() {
                     @Override
                     public void onItemClick(View view, int position) {
+                        Usuario usuarioSelecionado = listacontatos.get(position);
                         Intent i = new Intent(getActivity(), ChatActivity.class);
+                        i.putExtra("chatcontato",usuarioSelecionado);
                         startActivity(i);
                     }
 
@@ -136,14 +137,14 @@ public class ContatosFragment extends Fragment {
 
     public void recuperarContatos (){
 
+        listacontatos.clear();
        valueEventListenerContatos = usuarioref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 for (DataSnapshot dados: snapshot.getChildren()) {
-                    listacontatos.clear();
                     Usuario usuario = dados.getValue(Usuario.class);
                     String emailusueratual = usuarioatual.getEmail();
-                    if (usuario.getEmail() != emailusueratual) {
+                    if (!emailusueratual.equals(usuario.getEmail())) {
                         listacontatos.add(usuario);
                     }
                 }
